99139e3d96edba950000b63f8a9a40cc6fd5bc01
你们的小f <nmdfzf404@163.com>
2022-02-18 20:38:25 +0800

    :bookmark: v0.1.3

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,15 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true
+    },
+    builderOptions: {
+      artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
+      mac: {
+        target: {
+          arch: ['x64', 'arm64'],
+          target: 'dmg'
+        }
+      }
     }
   }
 }
