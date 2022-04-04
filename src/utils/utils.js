b8f43463c70ccfdd9e55d9e42114bf5c1c5a05bf
你们的小f <nmdfzf404@163.com>
2022-04-04 16:32:09 +0800

    🐛 变动详情的算法问题

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: fzf404
  * @Date: 2022-02-18 17:38:10
- * @LastEditTime: 2022-02-18 19:58:53
+ * @LastEditTime: 2022-04-04 16:26:06
  * @Description: 工具函数
  */
 
@@ -18,10 +18,15 @@ export const getArrDiff = (arr1, arr2) => {
   })
 }
 
+/**
+ * @description: 获取两个数组间的某个 key 的差集
+ * @param {*} arr1
+ * @param {*} arr2
+ */
 export const getArrDiffKey = (arr1, arr2, key) => {
   return arr2.filter((items) => {
     return !arr1.some((item) => {
-      return items['name'] === item['name'] && items[key] === item[key]
+      return items['repo'] === item['repo'] && items[key] === item[key]
     })
   })
 }
