9efdd6e9ee83048d62b94959848c456c41b97f65
你们的小f <nmdfzf404@163.com>
2022-05-30 17:40:43 +0800

    :sparkles: 增加了使用指引

---
+++ pages/plugins/welcome.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout />
+
+  <article>
+    <h1>欢迎使用</h1>
+
+    <p>请在托盘图标中找到 Monit</p>
+    <p>点击插件列表启动插件</p>
+
+    <button @click="openGithub">
+      <GithubSVG height="22" />
+    </button>
+  </article>
+</template>
+
+<script setup>
+import { shell } from 'electron'
+import GithubSVG from '../assets/github/github.svg'
+import Layout from '../layout/common.vue'
+
+// 打开链接
+const openGithub = () => shell.openExternal('https://github.com/fzf404/Monit')
+</script>
+
+<style scoped>
+article {
+  @apply h-screen pt-4 flex flex-col justify-center items-center space-y-3;
+}
+article h1 {
+  @apply text-lg text-white;
+}
+article p {
+  @apply text-sm items-center text-gray-400;
+}
+article button {
+  @apply p-1 outline-none rounded-md bg-purple-500 hover:bg-purple-700 text-purple-100 text-sm;
+}
+</style>
