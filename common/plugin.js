e6774c9253150d4dedb7ce3e136d94da9b3ca102
你们的小f <nmdfzf404@163.com>
2022-05-30 17:38:36 +0800

    :wrench: 重新设计了插件配置

--- common/plugin.js
+++ common/plugin.js
@@ -1,7 +1,17 @@
-const plugins = [
-  { width: 420, height: 200, name: 'github' },
-  { width: 420, height: 200, name: 'bilibili' },
-  { width: 420, height: 200, name: 'cloudmusic' },
-]
+/*
+ * @Author: fzf404
+ * @Description: 插件列表
+ */
 
-export default plugins
+/**
+ * @param {string} name 插件名称
+ * @param {Array} size [插件宽度, 插件高度]
+ * @param {string} description 插件描述
+ */
+export const pluginList = [
+  { name: 'welcome', size: [1, 1], description: '为新用户提供指引' },
+  { name: 'github', size: [2, 1], description: '监控自己的 github 信息' },
+  // { name: 'bilibili', size: [1, 1], description: '监控自己的 bilibili 信息' },
+  // { name: 'music', size: [2, 1], description: '播放网易云音乐' },
+  // { name: 'todo', size: [1, 2], description: '管理自己的待办事项' },
+]
