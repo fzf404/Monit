20cf808b9dff33cec405d0b2ff88a1fae66b5bc7
你们的小f <nmdfzf404@163.com>
2022-05-30 17:35:45 +0800

    :recycle: 重构了窗口管理器

--- app/main.js
+++ app/main.js
@@ -1,125 +1,74 @@
+/*
+ * @Author: fzf404
+ * @Description: 应用初始化
+ */
+
 import { app, protocol, BrowserWindow } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 import { autoUpdater } from 'electron-updater'
 
-import tray from './tray'
-import handleEvents from './event'
-import { cget } from '../common/storage'
+import { initTray, showTray } from './tray'
+import { appEvent } from './event'
+import { autoWindow, createWindow } from './window'
 
 // 调试模式
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // 注册协议
-protocol.registerSchemesAsPrivileged([{ scheme: 'monit', privileges: { secure: true, standard: true } }])
+protocol.registerSchemesAsPrivileged([{ scheme: 'Monit', privileges: { secure: true, standard: true } }])
 
-// 窗口跟踪
-const windows = new Set()
+// mac 激活窗口
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow('welcome')
+})
 
-// 创建窗口
-function createWindow(name) {
-  // 读取配置
-  const x = cget(name, 'x', 10)
-  const y = cget(name, 'y', 10)
-  const top = cget(name, 'top', false)
-
-  // 创建窗口
-  let win = new BrowserWindow({
-    x: x,
-    y: y,
-    width: 420,
-    height: 200,
-    alwaysOnTop: top, // 置顶
-    transparent: true, // 透明
-    hasShadow: false, // 阴影
-    frame: false, // 隐藏边框
-    fullscreenable: false, // 禁止全屏
-    resizable: false, // 不可调整大小
-
-    // opacity: 0.8, // 不透明度
-    // vibrancy: 'dark', // mac 毛玻璃
-    // visualEffectState: 'active', // mac 毛玻璃 自动应用
-
-    webPreferences: {
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
-    },
-  })
-
-  // 根据模式启动应用
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // 调试模式
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL + '#/' + name) // 加载应用
-    if (!process.env.IS_TEST) win.webContents.openDevTools() // 打开调试器
-  } else {
-    // 生产模式
-    createProtocol('app') // 创建协议
-    win.loadURL('app://./index.html#/' + name) // 加载应用
+// mac 关闭窗口
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
+})
 
-  win.on('closed', () => {
-    windows.delete(win)
-  })
+// 准备就绪
+app.on('ready', async () => {
+  // 初始化系统托盘
+  initTray()
 
-  // 监听事件
-  handleEvents(win, name)
+  // 应用事件监听
+  appEvent()
 
-  windows.add(win)
-}
+  // 延时启动窗口 用于修复 Linux 窗口不透明
+  setTimeout(() => {
+    // 自动打开窗口
+    autoWindow()
+  }, 300)
 
-// 初始化窗口
-function launchApp() {
-  // 调试模式下退出方法
-  if (isDevelopment) {
-    if (process.platform === 'win32') {
-      process.on('message', (data) => {
-        if (data === 'graceful-exit') {
-          app.quit()
-        }
-      })
-    } else {
-      process.on('SIGTERM', () => {
+  // 自动检查更新
+  autoUpdater.checkForUpdatesAndNotify()
+})
+
+// 调试模式下安装 vue-devtools
+app.on('ready', () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    try {
+      installExtension(VUEJS3_DEVTOOLS)
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString())
+    }
+  }
+})
+
+// 调试模式下退出
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
         app.quit()
-      })
-    }
-  }
-
-  // mac 激活窗口
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-
-  // mac 关闭窗口
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-
-  // 自动更新
-  app.on('ready', () => {
-    autoUpdater.checkForUpdatesAndNotify()
-  })
-
-  // 准备就绪
-  app.on('ready', async () => {
-    // 调试模式下安装 vue-devtools
-    if (isDevelopment && !process.env.IS_TEST) {
-      try {
-        await installExtension(VUEJS3_DEVTOOLS)
-      } catch (e) {
-        console.error('Vue Devtools failed to install:', e.toString())
       }
-    }
-    // 系统托盘
-    tray()
-    // 修复 Linux 无法透明窗口
-    setTimeout(() => {
-      createWindow('github')
-      createWindow('bilibili')
-    }, 300)
-  })
+    })
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit()
+    })
+  }
 }
-
-// 初始化窗口
-launchApp()
---
+++ app/window.js
@@ -0,0 +1,88 @@
+/*
+ * @Author: fzf404
+ * @Description: 窗口管理
+ */
+
+import { BrowserWindow } from 'electron'
+
+import { cget } from '../common/storage'
+import { pluginList } from '../common/plugin'
+import { winEvent } from './event'
+
+// 窗口网格大小
+const BasicMesh = 200
+
+// 窗口列表
+const windowList = []
+
+// 创建窗口
+export const createWindow = (name) => {
+  // 判断窗口是否存在
+  const exist = windowList.find((item) => item.title === name)
+  if (exist) {
+    // 展示窗口
+    exist.show()
+    return
+  }
+
+  // 窗口大小
+  const size = pluginList.find(({ name: n }) => n === name).size
+
+  // 读取配置
+  const x = cget(name, 'x', 20)
+  const y = cget(name, 'y', 20)
+  const top = cget(name, 'top', false)
+
+  // 创建窗口
+  const win = new BrowserWindow({
+    x: x,
+    y: y,
+    title: name,
+    width: size[0] * BasicMesh,
+    height: size[1] * BasicMesh,
+    alwaysOnTop: top, // 置顶
+
+    transparent: true, // 透明
+    hasShadow: false, // 阴影
+    frame: false, // 隐藏边框
+    resizable: false, // 不可调整大小
+    fullscreenable: false, // 禁止全屏
+    roundedCorners: false, // 圆角
+
+    webPreferences: {
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+    },
+  })
+
+  // 根据模式启动应用
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // 调试模式
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL + '#/' + name) // 加载应用
+    if (!process.env.IS_TEST) win.webContents.openDevTools() // 打开调试器
+  } else {
+    // 生产模式
+    createProtocol('app') // 创建协议
+    win.loadURL('app://./index.html#/' + name) // 加载应用
+  }
+
+  // 监听事件
+  winEvent(win, name)
+
+  // 移除窗口
+  win.on('closed', () => {
+    windowList.splice(windowList.indexOf(windowList.find((item) => item.title === name)), 1)
+    console.log(windowList)
+  })
+
+  // 添加窗口
+  windowList.push(win)
+}
+
+// 开机自启窗口
+export const autoWindow = () => {
+  const openPlugins = cget('_config', 'open', ['welcome'])
+  openPlugins.forEach((item) => {
+    createWindow(item)
+  })
+}
