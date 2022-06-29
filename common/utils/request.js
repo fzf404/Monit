7aebc098dc300664e65733ddeb3cc8f50e0ec9e6
你们的小f <nmdfzf404@163.com>
2022-06-29 21:46:15 +0800

    :bookmark: v0.4.1

--- common/utils/request.js
+++ common/utils/request.js
@@ -2,7 +2,7 @@
  * @Author: fzf404
  * @Date: 2022-05-18 23:06:12
  * @LastEditors: fzf404 nmdfzf404@163.com
- * @LastEditTime: 2022-06-19 00:42:53
+ * @LastEditTime: 2022-06-29 11:01:54
  * @Description: axios 请求封装
  */
 
@@ -14,7 +14,7 @@ const request = (url) => {
     baseURL: url, // 基本路径
     timeout: 30000, // 超时时间
   })
-  // 返回拦截
+  // 响应拦截
   service.interceptors.response.use(
     (res) => {
       // 返回数据
