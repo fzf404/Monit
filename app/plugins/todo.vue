132baf5347dfec58f59f8ca309ffb7a2fcd60d34
你们的小f <nmdfzf404@163.com>
2022-08-11 22:27:15 +0800

    :lipstick: 调整 todo 样式

--- app/plugins/todo.vue
+++ app/plugins/todo.vue
@@ -2,26 +2,25 @@
  * @Author: fzf404
  * @Date: 2022-05-26 17:37:12
  * @LastEditors: fzf404 nmdfzf404@163.com
- * @LastEditTime: 2022-07-22 16:12:15
+ * @LastEditTime: 2022-08-11 16:07:45
  * @Description: todo 代办事项管理
 -->
 
 <template lang="pug">
 main
   Layout
-  article.h-screen.p-4.pt-7
-    draggable.flex.flex-col.items-start.h-56.overflow-hidden.overflow-y-scroll( tag="ul" :list="todos" handle=".handle" :animation="200" item-key="id")
+  article.h-screen.flex.flex-col.justify-between.pt-8.pl-4.pr-3.pb-3
+    draggable.flex.flex-col.items-start.overflow-hidden.overflow-y-scroll(class="space-y-1" tag="ul" :list="todos" handle=".handle" :animation="200" item-key="id")
       template(#item="{element,index}")
-        li.flex.items-center(class="space-y-1")
-          input.h-4.w-4.mr-2.list-item.accent-purple-500(v-model="element.checked" type="checkbox")
-          input.w-28.bg-transparent.outline-none.text-sm(:class="element.checked?'line-through text-gray-400':'text-gray-200'" v-model="element.title" type="text")
-          DeleteSVG.w-4.stroke-current.clickable.text-rose-400(class="hover:text-rose-500" @click="remove(index)" )
-          MoveSVG.w-5.stroke-current.clickable.text-purple-400.handle(class="hover:text-purple-500")
-    footer.flex.items-center.pt-2.border-t-2.border-indigo-500
-      input.h-4.w-4.accent-purple-500.mr-2( type="checkbox")
-     
-      input.w-24.bg-transparent.outline-none.mr-5.text-sm( v-model="todo" @keyup.enter="add" type="text")
-      AddSVG.w-4.stroke-current.clickable.text-cyan-400(class="hover:text-cyan-500" @click="add")
+        li.flex.items-center
+          input.mr-2.accent-purple-500(v-model="element.checked" type="checkbox")
+          input.mr-2.w-full.bg-transparent.outline-none.text-sm(:class="element.checked?'line-through text-gray-400':'text-gray-200'" v-model="element.title" type="text")
+          DeleteSVG.w-6.stroke-current.clickable.text-rose-400(class="hover:text-rose-500" @click="remove(index)" )
+          MoveSVG.w-7.stroke-current.clickable.text-purple-400.handle(class="hover:text-purple-500")
+    footer.flex.items-center.border-t-2.border-indigo-500.pt-2
+      input.mr-2.accent-purple-500( type="checkbox")
+      input.mr-2.w-full.bg-transparent.outline-none.text-sm( v-model="todo" @keyup.enter="add" type="text")
+      AddSVG.w-5.mr-2.stroke-current.clickable.text-cyan-400(class="hover:text-cyan-500" @click="add")
 </template>
 
 <script setup>
