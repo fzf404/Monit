2d6559822ba311caa2b3921b4a4b417cd25ec6db
你们的小f <nmdfzf404@163.com>
2022-09-23 20:31:37 +0800

    :sparkles: 完成 image 组件

---
+++ app/components/image.vue
@@ -0,0 +1,13 @@
+<template>
+  <img
+    v-show="props.show"
+    class="absolute w-full h-full z-50 modal object-contain pt-3 pb-12"
+    :src="props.image"
+    alt="图像"
+  />
+  <p class="absolute w-full z-50 text-center bottom-3 bg-theme text-lg">{{props.remark}}</p>
+</template>
+
+<script setup>
+const props = defineProps(['show', 'image','remark'])
+</script>
