5794d9fcd2c5dce8b518853f85fae44d06ed3da6
你们的小f <nmdfzf404@163.com>
2022-09-24 23:46:13 +0800

    :lipstick: 调整主题样式

--- app/components/image.vue
+++ app/components/image.vue
@@ -1,13 +1,10 @@
 <template>
-  <img
-    v-show="props.show"
-    class="absolute w-full h-full z-50 modal object-contain pt-3 pb-12"
-    :src="props.image"
-    alt="图像"
-  />
-  <p class="absolute w-full z-50 text-center bottom-3 bg-theme text-lg">{{props.remark}}</p>
+  <article v-show="show">
+    <img class="absolute w-full h-full z-50 modal object-contain pt-3 pb-12" :src="image" alt="图像" />
+    <p class="absolute w-full z-50 text-center bottom-3 bg-theme text-lg">{{ remark }}</p>
+  </article>
 </template>
 
 <script setup>
-const props = defineProps(['show', 'image','remark'])
+const props = defineProps(['show', 'image', 'remark'])
 </script>
