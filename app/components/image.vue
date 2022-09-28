581019a313fefdc9b1e2c5b056264a6bf94e856d
你们的小f <nmdfzf404@163.com>
2022-09-28 22:58:41 +0800

    :lipstick: 更新 image 组件

--- app/components/image.vue
+++ app/components/image.vue
@@ -1,8 +1,15 @@
+<!--
+ * @Author: fzf404
+ * @Date: 2022-09-23 20:37:31
+ * @LastEditors: fzf404 nmdfzf404@163.com
+ * @LastEditTime: 2022-09-28 22:58:52
+ * @Description: iamge 组件
+-->
 <template>
-  <article v-show="show">
-    <img class="absolute w-full h-full z-50 modal object-contain pt-3 pb-12" :src="image" alt="图像" />
-    <p class="absolute w-full z-50 text-center bottom-3 bg-theme text-lg">{{ remark }}</p>
-  </article>
+  <section v-show="show" class="modal z-30 flex-col-center space-y-2">
+    <img class="h-2/3" :src="image" alt="图像" />
+    <p class="w-full bg-theme text-center text-lg">{{ remark }}</p>
+  </section>
 </template>
 
 <script setup>
