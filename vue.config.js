64417328860af1ac758e95977f26a55b474f86ac
你们的小f <me@fzf404.art>
2023-03-23 17:19:51 +0800

    :sparkles: 路径缩写

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,17 @@
 const path = require('path')
 
-function resolve(dir) {
+const resolve = (dir) => {
   return path.join(__dirname, dir)
 }
 
 const config = {
   configureWebpack: {
     // electron 入口
-    entry: './core/main.js',
+    entry: './core/main.js'
   },
   chainWebpack: (config) => {
     // import 别名
-    config.resolve.alias
-      .set('@', resolve('app'))
-      .set('~', resolve('lib'))
-      .set('#', resolve('custom'))
-      .set('core', resolve('core'))
-      .set('root', resolve('./'))
+    config.resolve.alias.set('~', resolve('./')).set('@', resolve('app'))
     // svg 加载
     config.module.rule('svg').delete('type').use('svg-vue3-loader').loader('svg-vue3-loader')
   },
@@ -33,14 +28,8 @@ const config = {
       mainProcessFile: 'core/main.js',
       // 渲染进程入口
       rendererProcessFile: 'app/main.js',
-      // 路径别名
       chainWebpackMainProcess: (config) => {
-        config.resolve.alias
-          .set('@', resolve('app'))
-          .set('~', resolve('lib'))
-          .set('#', resolve('custom'))
-          .set('core', resolve('core'))
-          .set('root', resolve('./'))
+        config.resolve.alias.set('~', resolve('./'))
       },
       // 构建选项
       builderOptions: {
@@ -49,37 +38,37 @@ const config = {
         // 图标
         icon: 'public/icons/icon.png',
         // 应用 ID
-        appId: 'art.fzf404.monit',
+        appId: 'monit.fzf404.art',
         // 打包命名
         artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
         // 发布地址
         publish: ['github'],
         // 多平台配置
         win: {
-          target: ['nsis', '7z'],
+          target: ['nsis', '7z']
         },
         linux: {
-          target: 'AppImage',
+          target: 'AppImage'
         },
         mac: {
           target: {
             target: 'dmg',
-            arch: ['x64', 'arm64'],
+            arch: ['x64', 'arm64']
           },
           entitlements: 'entitlements.mac.plist',
           extendInfo: {
             NSMicrophoneUsageDescription: '请提供您的麦克风权限！',
-            NSCameraUsageDescription: '请提供您的相机权限！',
-          },
-        },
-      },
-    },
-  },
+            NSCameraUsageDescription: '请提供您的相机权限！'
+          }
+        }
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   // 热重载配置
-  config.pluginOptions.electronBuilder.mainProcessWatch = ['core/*.js', 'custom/*.ts']
+  config.pluginOptions.electronBuilder.mainProcessWatch = ['config/*.ts', 'core/*.js', 'server/*.ts']
 }
 
 module.exports = { ...config }
