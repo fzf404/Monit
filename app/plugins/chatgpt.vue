502f01f504b99849d93fb672fe6db37438de4802
fzf404 <me@fzf404.art>
2023-03-29 22:24:32 +0800

    :sparkles: 引入 chatgpt

---
+++ app/plugins/chatgpt.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: fzf404
+ * @Date: 2022-05-25 23:18:50
+ * @LastEditors: fzf404 me@fzf404.art
+ * @LastEditTime: 2023-03-29 21:12:34
+ * @Description: chatgpt 对话机器人
+-->
+<template>
+  <!-- 设置-->
+  <Setting />
+  <!-- 页面内容 -->
+  <article class="flex-col-center">
+    <h1 class="text-light text-lg">ChatGPT</h1>
+  </article>
+</template>
+
+<!-- TODO 插件 chatgpt -->
+<script setup>
+import Setting from '@/components/setting.vue'
+</script>
