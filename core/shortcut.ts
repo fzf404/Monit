16185b171f6ff36ad61c921b4ff66c5eb9387441
你们的小f <me@fzf404.art>
2023-03-30 09:19:55 +0800

    :sparkles: 注册全局快捷键

--- core/shortcut.ts
+++ core/shortcut.ts
@@ -2,7 +2,7 @@
  * @Author: fzf404
  * @Date: 2023-03-11 22:15:41
  * @LastEditors: fzf404 me@fzf404.art
- * @LastEditTime: 2023-03-29 21:13:53
+ * @LastEditTime: 2023-03-30 09:18:36
  * @Description: shortcut 初始化
  */
 
@@ -12,8 +12,8 @@ import { createPlugin } from '~/server/plugin'
 
 // 初始化快捷键
 export const initShortcut = () => {
-  // 注册快捷键
-  globalShortcut.register('CommandOrControl+,', () => {
+  // 注册全局快捷键
+  globalShortcut.register('CommandOrControl+Shift+m', () => {
     // 打开配置插件
     createPlugin('config')
   })
