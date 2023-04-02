57b4f68cd6913620eb3c16a32ad040e4f8052704
你们的小f <me@fzf404.art>
2023-04-03 00:27:31 +0800

    :fire: 更改 statistic 参数名

--- lib/statistic.ts
+++ lib/statistic.ts
@@ -2,30 +2,37 @@
  * @Author: fzf404
  * @Date: 2022-05-18 23:06:12
  * @LastEditors: fzf404 me@fzf404.art
- * @LastEditTime: 2023-03-28 09:42:21
+ * @LastEditTime: 2023-04-02 23:57:16
  * @Description: statistic 数据分析
  */
 
 /**
  * @description: 获取两个数组间的差集
- * @param { Array<T> } arr1 源数组
- * @param { Array<T> } arr2 目标数组
+ * @param { Array<T> } source 源数组
+ * @param { Array<T> } target 目标数组
  * @return { Array<T> } 数组差集
  */
-export const getArrDiff = <T>(arr1: Array<T>, arr2: Array<T>): Array<T> => {
-  return arr2.filter((item) => !arr1.includes(item))
+export const getArrDiff = <T>(source: Array<T>, target: Array<T>): Array<T> => {
+  return target.filter((item) => !source.includes(item))
 }
 
 /**
- * @description: 获取两个数组间的某个 key 的差集
- * @param { Array<T> } arr1 源数组
- * @param { Array<T> } arr2 目标数组
+ * @description: 获取两个数组间的某个值的差集
+ * @param { Array<T> } sourceArray 源数组
+ * @param { Array<T> } targetArray 目标数组
+ * @param { keyof T } sourceKey 源数组的某个值
+ * @param { keyof T } targetKey 目标数组的某个值
  * @return { Array<T> } 数组差集
  */
-export const getArrDiffKey = <T>(arr1: Array<T>, arr2: Array<T>, source: keyof T, target: keyof T): Array<T> => {
-  return arr2.filter((items) => {
-    return !arr1.some((item) => {
-      return items[source] === item[source] && items[target] === item[target]
+export const getArrDiffKey = <T>(
+  sourceArray: Array<T>,
+  targetArray: Array<T>,
+  sourceKey: keyof T,
+  targetKey: keyof T
+): Array<T> => {
+  return targetArray.filter((target) => {
+    return !sourceArray.some((source) => {
+      return target[sourceKey] === source[sourceKey] && target[targetKey] === source[targetKey]
     })
   })
 }
