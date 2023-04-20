70c95dc80060a7eeeb27b289740e961db9e4c136
fzf404 <me@fzf404.art>
2023-04-20 22:26:11 +0800

    :sparkles: 聚焦快捷键

--- core/shortcut.ts
+++ core/shortcut.ts
@@ -2,19 +2,23 @@
  * @Author: fzf404
  * @Date: 2023-03-11 22:15:41
  * @LastEditors: fzf404 me@fzf404.art
- * @LastEditTime: 2023-03-30 09:18:36
+ * @LastEditTime: 2023-04-20 21:43:58
  * @Description: shortcut 初始化
  */
 
 import { globalShortcut } from 'electron'
 
-import { createPlugin } from '~/server/plugin'
+import { createPlugin, focusAllPlugin } from '~/server/plugin'
 
 // 初始化快捷键
 export const initShortcut = () => {
-  // 注册全局快捷键
+  // 激活全部插件
+  globalShortcut.register('CommandOrControl+Shift+Alt+m', () => {
+    focusAllPlugin()
+  })
+
+  // 开启配置插件
   globalShortcut.register('CommandOrControl+Shift+m', () => {
-    // 打开配置插件
     createPlugin('config')
   })
 }
