ca328e8b59f97f1e8e89537c4b8fb169ac363c16
fzf404 <me@fzf404.art>
2023-08-27 23:30:59 +0800

    refactor: move unocss config

--- app/unocss.config.ts
+++ app/unocss.config.ts
@@ -2,6 +2,11 @@ import { defineConfig, presetIcons, presetUno } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
+  shortcuts: {
+    'flex-row-center': 'flex flex-row justify-center',
+    'flex-row-between': 'flex flex-row justify-between',
+    'flex-col-center': 'flex flex-col items-center justify-center',
+  },
   presets: [
     presetUno(),
     presetIcons({
