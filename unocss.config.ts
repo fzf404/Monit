d2092e31b697ee75dcb6b3f1e8ff3086811a1f2a
fzf404 <me@fzf404.art>
2023-08-27 13:29:32 +0800

    feat: unocss icons preset

--- unocss.config.ts
+++ unocss.config.ts
@@ -2,6 +2,14 @@ import { defineConfig, presetIcons, presetUno } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
-  presets: [presetUno(), presetIcons()],
+  presets: [
+    presetUno(),
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+  ],
   transformers: [transformerDirectives()],
 })
