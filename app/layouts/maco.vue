b096103dcc0ff9c3cafb3e41504ae83f9a60d6bb
fzf404 <me@fzf404.art>
2023-08-27 23:34:40 +0800

    feat: plugin basic style

--- app/layouts/maco.vue
+++ app/layouts/maco.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import i18n from '@/i18n'
+import i18n from '@/configs/locale'
 
 const { locale } = i18n.global
 
@@ -11,30 +11,41 @@ const toggleLanguage = () => {
 <template>
   <nav class="flex-row-between absolute z-50 px-2 py-1.5">
     <ul class="flex-row-center gap-x-0.5">
-      <div class="i-ic-twotone-cancel text-red hover:text-red-500" />
-      <div class="i-ic-twotone-remove-circle text-yellow hover:text-yellow-500" />
-      <div class="i-ic-twotone-expand-circle-down text-green hover:text-green-500" />
+      <svg class="i-ic-twotone-cancel text-red hover:text-red-500"></svg>
+      <svg class="i-ic-twotone-remove-circle text-yellow hover:text-yellow-500"></svg>
+      <svg class="i-ic-twotone-expand-circle-down text-green hover:text-green-500"></svg>
     </ul>
 
     <ul class="flex-row-center gap-x-0.5">
-      <div
+      <svg
         class="i-ic-twotone-swap-horizontal-circle text-teal hover:text-teal-500"
         @click="toggleLanguage"
-      />
-      <div class="i-ic-twotone-dark-mode text-violet hover:text-violet-500" />
-      <div class="i-ic-twotone-settings text-blue hover:text-blue-500" />
+      ></svg>
+      <svg class="i-ic-twotone-dark-mode text-violet hover:text-violet-500"></svg>
+      <svg class="i-ic-twotone-settings text-blue hover:text-blue-500"></svg>
     </ul>
   </nav>
 </template>
 
-<style>
-.flex-row-between {
-  @apply flex flex-row justify-between;
-}
-.flex-row-center {
-  @apply flex flex-row justify-center;
-}
-.flex-col-center {
-  @apply flex flex-col items-center justify-center;
+<style lang="scss">
+body {
+  -webkit-app-region: drag;
+  user-select: none;
+
+  * {
+    @apply transition;
+  }
+
+  img {
+    -webkit-user-drag: none;
+  }
+
+  svg,
+  input,
+  select,
+  button {
+    -webkit-app-region: no-drag;
+    cursor: pointer;
+  }
 }
 </style>
