de8c8efe98728132334ec37210bb770a4d9c521d
fzf404 <me@fzf404.art>
2023-08-27 13:48:18 +0800

    feat: maco layout icons

--- app/layouts/index.vue
+++ app/layouts/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import MacoLayout from './maco.vue'
+</script>
+
 <template>
+  <MacoLayout class="w-screen" />
   <router-view class="h-screen" />
 </template>
+
+<style>
+.flex-row-between {
+  @apply flex flex-row justify-between;
+}
+</style>
---
+++ app/layouts/maco.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="flex-row-between absolute z-50 px-2">
+    <ul class="space-x-0.5">
+      <div class="i-ic-twotone-cancel bg-red hover:bg-red-500" />
+      <div class="i-ic-twotone-remove-circle bg-yellow hover:bg-yellow-500" />
+      <div class="i-ic-twotone-expand-circle-down bg-green hover:bg-green-500" />
+    </ul>
+    <ul class="space-x-0.5">
+      <div class="i-ic-twotone-swap-horizontal-circle bg-teal hover:bg-teal-500" />
+      <div class="i-ic-twotone-dark-mode bg-violet hover:bg-violet-500" />
+      <div class="i-ic-twotone-settings bg-blue hover:bg-blue-500" />
+    </ul>
+  </nav>
+</template>
