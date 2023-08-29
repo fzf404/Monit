ec49e0b6d057cb59a3947fc8e185bc899c273e2f
fzf404 <me@fzf404.art>
2023-08-29 21:44:06 +0800

    refactor: electron context bridge

--- context/preload.ts
+++ context/preload.ts
@@ -1,12 +1,16 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
+const api = {
+  ...electronAPI.ipcRenderer,
+}
+
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
   } catch (error) {
     console.error(error)
   }
 } else {
-  window.electron = electronAPI
+  window.api = api
 }
