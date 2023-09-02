14434afe534e3f410e01d0f40fdeaf91c16f30fd
fzf404 <me@fzf404.art>
2023-09-02 16:52:17 +0800

    refactor: transtion anime

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useState } from '@/configs/state'
+import { initWatch } from '@/configs/watch'
 import Layout from '@/layouts/index.vue'
+
+initWatch(useState())
 </script>
 
 <template>
--- app/layouts/index.vue
+++ app/layouts/index.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-import { initWatch } from '@/configs/watch'
-
-import { useState } from '../configs/state'
 import MacoLayout from './maco.vue'
-
-const state = useState()
-
-initWatch(state)
 </script>
 
 <template>
-  <Transition v-show="state.navbar.show" name="slide-fade">
-    <MacoLayout class="w-screen" />
-  </Transition>
+  <MacoLayout class="w-screen" />
   <router-view class="h-screen" />
 </template>
