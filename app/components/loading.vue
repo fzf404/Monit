5689feed6ebd1ee0079c63970bb5f370a9712212
fzf404 <me@fzf404.art>
2023-09-12 16:50:39 +0800

    feat: loading component

---
+++ app/components/loading.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  show: boolean
+  remark?: string | string[]
+}
+
+withDefaults(defineProps<Props>(), {
+  remark: '加载中...',
+})
+</script>
+
+<template>
+  <section
+    v-show="show"
+    class="absolute z-30 h-full w-full flex-col-center gap-2 rounded-lg bg-gray-300 bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-50"
+  >
+    <svg
+      class="load-rotating i-ic-twotone-change-circle h-16 w-16 text-gray-200 dark:text-gray-300"
+    ></svg>
+    <p
+      v-if="typeof remark === 'string'"
+      class="font-mono text-gray-200 dark:text-gray-300"
+    >
+      {{ remark }}
+    </p>
+    <p v-for="(item, index) in remark" v-else :key="index" class="font-intro">
+      {{ item }}
+    </p>
+  </section>
+</template>
