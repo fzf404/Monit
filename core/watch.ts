90ccda0190cc38a11e97fdef4e2570c0d110d27a
fzf404 <me@fzf404.art>
2023-09-18 18:25:47 +0800

    feat: watch window event

--- core/watch.ts
+++ core/watch.ts
@@ -5,4 +5,11 @@ export const initWatch = ({ window, storage }: PluginOptions) => {
     const [x, y] = window.getPosition()
     storage.set('config', { x, y })
   })
+  window.on('resize', () => {
+    const [width, height] = window.getSize()
+    storage.set('config', { width, height })
+  })
+  window.on('blur', () => {
+    window.webContents.send('set-plugin-navbar', false)
+  })
 }
