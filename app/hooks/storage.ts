bcfad3bb5f2f9ae01be41d01328ad3c9904fe245
fzf404 <me@fzf404.art>
2023-10-13 00:37:32 +0800

    feat: storage hook

--- app/hooks/storage.ts
+++ app/hooks/storage.ts
@@ -3,15 +3,22 @@ import { reactive, watch } from 'vue'
 
 type Storage = Record<string, any>
 
-export const useStorage = <T extends Storage>(data: T): UnwrapNestedRefs<T> => {
-  const storage = reactive(data)
+export const useStorage = async <T extends Storage>(
+  store: T,
+): Promise<UnwrapNestedRefs<T>> => {
+  const data = await window.api?.invoke('get-plugin-data', 'storage')
 
-  watch(
-    () => storage,
-    () => {
-      window.api?.invoke('set-plugin-data', 'storage', storage)
-    },
-  )
+  for (const key in store) {
+    if (data && data[key] !== undefined) {
+      store[key] = data[key]
+    }
+  }
+
+  const storage = reactive(store)
+
+  watch(storage, (data) => {
+    window.api?.invoke('set-plugin-data', 'storage', { ...data })
+  })
 
   return storage
 }
