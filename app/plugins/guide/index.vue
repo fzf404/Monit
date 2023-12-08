15bb1cb2a4e0adb0b27b1057f87813797eb674a1
xiaobu <liz3002@126.com>
2023-12-08 17:56:47 +0800

    temp:loading

--- app/plugins/guide/index.vue
+++ app/plugins/guide/index.vue
@@ -2,30 +2,24 @@
 import { useI18n } from 'vue-i18n'
 
 import MonitSVG from '@/assets/monit.svg'
-import Loading from '@/components/qrcode.vue'
+// import Loading from '@/components/qrcode.vue'
 
 const { t } = useI18n()
 </script>
 
 <template>
-  <Loading :show="true" image="https://img.fzf404.art/monit/qrcode.webp">
-    <p>123</p>
-  </Loading>
+  <!--  <Loading :show="true" image="https://img.fzf404.art/monit/qrcode.webp">-->
+  <!--    <p>123</p>-->
+  <!--  </Loading>-->
   <section class="flex-col-center gap-y-3 p-4 pt-9">
     <h1 class="text-lg text-gray-200">{{ t('title') }}</h1>
-    <i18n-t
-      keypath="tray"
-      tag="p"
-      class="flex-row-center text-sm text-gray-300"
-    >
+    <i18n-t keypath="tray" tag="p" class="flex-row-center text-sm text-gray-300">
       <img class="inline-block h-5 px-1" :src="MonitSVG" />
     </i18n-t>
     <p class="text-sm text-gray-400">
       {{ t('button') }}
     </p>
-    <button
-      class="rounded bg-purple p-1 opacity-80 dark:bg-purple-600 hover:opacity-100"
-    >
+    <button class="bg-purple rounded p-1 opacity-80 hover:opacity-100 dark:bg-purple-600">
       <svg class="i-ic-twotone-settings block h-7 w-7 bg-white"></svg>
     </button>
   </section>
